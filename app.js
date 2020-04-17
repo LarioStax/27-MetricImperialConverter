3bdcac959b2f1e9bfd6a08d4984568abfeb698d8
StaxGit <lario.free@gmail.com>
2020-04-17 09:57:52 +0200

    Add FCC Test starter

--- app.js
+++ app.js
@@ -23,4 +23,16 @@ routes(app);
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
 	console.log("Listening on port " + port + "!");
+  if(process.env.NODE_ENV==='test') {
+    console.log('Running Tests...');
+    setTimeout(function () {
+      try {
+        runner.run();
+      } catch(e) {
+        var error = e;
+          console.log('Tests are not valid:');
+          console.log(error);
+      }
+    }, 1500);
+  }
 });
